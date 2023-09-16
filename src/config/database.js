b30e9c69a6362123c2823a40cc3ee7542f4f60f6
Weslei França <weslwifranca@gmail.com>
2023-09-15 23:49:05 -0300

    update database,js

--- src/config/database.js
+++ src/config/database.js
@@ -1,33 +1,33 @@
 import pg from 'pg';
 
-// export default (Sequelize) => {
-//   return {
-//     dialect: 'postgres',
-//     dialectModule: pg,
-//     host: process.env.HOST,
-//     username: process.env.USERNAME,
-//     password: process.env.PASSWORD,
-//     database: process.env.DATABASE,
-//     define: {
-//       timespamps: true,
-//       underscored: true,
-//       underscoredAll: true,
-//     },
-//   };
-// };
+export default (Sequelize) => {
+  return {
+    dialect: 'postgres',
+    dialectModule: pg,
+    host: process.env.HOST,
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    define: {
+      timespamps: true,
+      underscored: true,
+      underscoredAll: true,
+    },
+  };
+};
 
- export default (Sequelize) => {
-     return {
-       dialect: 'postgres',
-       dialectModule: pg,
-       host: 'localhost',
-       username: 'postgres',
-       password: 'postgres',
-       database: 'autoescola-postgres',
-       define: {
-         timespamps: true,
-         underscored: true,
-         underscoredAll: true,
-       },
-     };
-   };
+//  export default (Sequelize) => {
+//      return {
+//        dialect: 'postgres',
+//        dialectModule: pg,
+//        host: 'localhost',
+//        username: 'postgres',
+//        password: 'postgres',
+//        database: 'autoescola-postgres',
+//        define: {
+//          timespamps: true,
+//          underscored: true,
+//          underscoredAll: true,
+//        },
+//      };
+//    };
